b8a0c2ec98fa445d05cd289c67a70c860effbb35
kprism <mstclma00@gmail.com>
2026-08-04 16:46:55 +0900

    Add publishing batch list view

--- publishing/views.py
+++ publishing/views.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from .models import PublishingBatch
+
+
+@login_required
+def batch_list(request):
+    batches = (
+        PublishingBatch.objects.filter(owner=request.user)
+        .prefetch_related("contents", "channels__platform")
+        .order_by("-created_at")
+    )
+    return render(request, "publishing/batch_list.html", {"batches": batches})
