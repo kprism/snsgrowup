05947dd6e53f080c2dff6fecddf9884d0311daf3
kprism <mstclma00@gmail.com>
2026-08-04 20:14:31 +0900

    Add RSS collection status endpoint

--- press_accounts/urls.py
+++ press_accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", views.rss_dashboard, name="rss_dashboard"),
     path("check/", views.rss_check, name="rss_check"),
     path("collect/", views.rss_collect, name="rss_collect"),
+    path("collect/status/<str:task_id>/", views.rss_collect_status, name="rss_collect_status"),
 ]
