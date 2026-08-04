dd457fe55cd1144230db28628b076694f9651324
kprism <mstclma00@gmail.com>
2026-08-04 17:36:55 +0900

    Use a fixed Facebook OAuth callback route

--- social_channels/urls.py
+++ social_channels/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path("<int:pk>/edit/", views.account_update, name="account_update"),
     path("<int:pk>/delete/", views.account_delete, name="account_delete"),
     path("<int:pk>/facebook/connect/", views.facebook_connect, name="facebook_connect"),
-    path("<int:pk>/facebook/callback/", views.facebook_callback, name="facebook_callback"),
+    path("facebook/callback/", views.facebook_callback, name="facebook_callback"),
     path("<int:pk>/facebook/pages/", views.facebook_page_select, name="facebook_page_select"),
     path("<int:pk>/facebook/disconnect/", views.facebook_disconnect, name="facebook_disconnect"),
 ]
