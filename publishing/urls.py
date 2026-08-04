7e1b650b91d18d7aa153744beeb3f9fee840e3c8
kprism <mstclma00@gmail.com>
2026-08-04 20:33:00 +0900

    Add manual Facebook task execute route

--- publishing/urls.py
+++ publishing/urls.py
@@ -7,5 +7,6 @@ app_name = "publishing"
 urlpatterns = [
     path("", views.batch_list, name="batch_list"),
     path("<int:pk>/", views.batch_detail, name="batch_detail"),
+    path("tasks/<int:pk>/execute/", views.task_execute, name="task_execute"),
     path("tasks/<int:pk>/retry/", views.task_retry, name="task_retry"),
 ]
