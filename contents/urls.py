7748af2ace84f6ccb10ede1fa7400abc9041c381
kprism <mstclma00@gmail.com>
2026-08-04 19:34:07 +0900

    Add Facebook preview route

--- contents/urls.py
+++ contents/urls.py
@@ -6,6 +6,7 @@ app_name = "contents"
 
 urlpatterns = [
     path("", views.content_list, name="content_list"),
+    path("facebook-preview/", views.facebook_preview, name="facebook_preview"),
     path("add/", views.content_create, name="content_create"),
     path("<int:pk>/edit/", views.content_update, name="content_update"),
     path("<int:pk>/delete/", views.content_delete, name="content_delete"),
