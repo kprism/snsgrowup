720cfe6810b5c0fe6872ceab735a4126d42b0fac
kprism <mstclma00@gmail.com>
2026-08-04 17:02:05 +0900

    Add publishing detail and retry routes

--- publishing/urls.py
+++ publishing/urls.py
@@ -6,4 +6,6 @@ app_name = "publishing"
 
 urlpatterns = [
     path("", views.batch_list, name="batch_list"),
+    path("<int:pk>/", views.batch_detail, name="batch_detail"),
+    path("tasks/<int:pk>/retry/", views.task_retry, name="task_retry"),
 ]
