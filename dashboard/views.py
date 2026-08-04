e00370b1ab3496951790a7dd7d7f3e5abf3c88d5
kprism <mstclma00@gmail.com>
2026-08-04 16:04:59 +0900

    Add account-aware dashboard context

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "dashboard/index.html")
+    context = {"account_type": None, "press_profile": None}
+    if request.user.is_authenticated:
+        context["account_type"] = request.user.account_type
+        if request.user.account_type == "press":
+            context["press_profile"] = getattr(request.user, "press_profile", None)
+    return render(request, "dashboard/index.html", context)
