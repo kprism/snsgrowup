3b7aabd5ada967adafe20c3b8c0e729f5dd77ff0
kprism <mstclma00@gmail.com>
2026-08-05 11:08:19 +0900

    Schedule publish queue dispatcher

--- config/celery.py
+++ config/celery.py
@@ -7,3 +7,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 app = Celery("snsgrowup")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    "dispatch-due-publish-queues": {
+        "task": "publishing.tasks.dispatch_due_publish_queues",
+        "schedule": 10.0,
+    },
+}
