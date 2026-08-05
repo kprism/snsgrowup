02e32b3eec3cdc43bc4795f8639aeba40d1b7d4c
kprism <mstclma00@gmail.com>
2026-08-05 10:58:58 +0900

    Support legacy automation settings URL

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("", include("dashboard.urls")),
@@ -9,6 +10,11 @@ urlpatterns = [
     path("channels/", include("social_channels.urls")),
     path("contents/", include("contents.urls")),
     path("publishing/", include("publishing.urls")),
+    path(
+        "settings/automation/",
+        RedirectView.as_view(url=reverse_lazy("publishing:automation_settings"), permanent=False),
+        name="automation_settings_legacy",
+    ),
     path("rss/", include("press_accounts.urls")),
     path("admin/", admin.site.urls),
 ]
